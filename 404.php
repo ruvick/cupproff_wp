e5f34b2b0efcd72455b3d9b7a1e952bb678c2b2f
Ruvick <rudikov.web@ya.ru>
2022-02-15 16:34:03 +0300

    Страница 404

--- 404.php
+++ 404.php
@@ -3,10 +3,35 @@
 
 <?php get_template_part('template-parts/header-section');?>
 
-	<main id="primary" class="page site-main">
+<main class="page page-recurring">
 
-	<div class = "content contentInPage centrElem">	 
-		<div class="_container">
+	<div class="bg-img bg-img-01">
+		<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/01.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/01.png?_v=1644843902057" alt=""></picture>
+	</div>
+	<div class="bg-img bg-img-02">
+		<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/02.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/02.png?_v=1644843902057" alt=""></picture>
+	</div>
+	<div class="bg-img bg-img-03">
+		<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/03.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/03.png?_v=1644843902057" alt=""></picture>
+	</div>
+	<!-- 			<div class="bg-img bg-img-04">
+		<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/04.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/04.png?_v=1644843902057" alt=""></picture>
+	</div> -->
+	<div class="bg-img bg-img-05">
+		<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/05.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/05.png?_v=1644843902057" alt=""></picture>
+	</div>
+	<div class="bg-img bg-img-06">
+		<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/06.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/06.png?_v=1644843902057" alt=""></picture>
+	</div>
+
+	<section class="recurring content">
+		<div class="_container"> 
+  		<?php
+				if ( function_exists('yoast_breadcrumb') ) {
+					yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );  
+				}
+			?> 
+
 			<h1 class = "h404">404</h1>
 				Запрашиваемая страница не была найдена
 		</div>
