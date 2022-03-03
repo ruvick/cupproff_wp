5c63f25c715366efe18bf40a8386f8d249a52f00
Ruvick <rudikov.web@ya.ru>
2022-03-03 16:57:41 +0300

    Убрал фоновые и поправил тень

--- single-ultra.php
+++ single-ultra.php
@@ -12,25 +12,6 @@ get_header(); ?>
 
 <main class="page page-recurring">
 
-<!-- <div class="bg-img bg-img-01">
-				<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/01.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/01.png?_v=1644843902057" alt=""></picture>
-			</div>
-			<div class="bg-img bg-img-02">
-				<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/02.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/02.png?_v=1644843902057" alt=""></picture>
-			</div>
-			<div class="bg-img bg-img-03">
-				<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/03.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/03.png?_v=1644843902057" alt=""></picture> 
-			</div> -->
-<!-- <div class="bg-img bg-img-04">
-				<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/04.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/04.png?_v=1644843902057" alt=""></picture>
-			</div> -->
-			<div class="bg-img bg-img-05">
-				<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/05.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/05.png?_v=1644843902057" alt=""></picture>
-			</div>
-			<div class="bg-img bg-img-06">
-				<picture><source srcset="<?php echo get_template_directory_uri();?>/img/bg-img/06.webp" type="image/webp"><img src="<?php echo get_template_directory_uri();?>/img/bg-img/06.png?_v=1644843902057" alt=""></picture>
-			</div>
-
 			<section id="product-sec" class="product-sec recurring">
 				<div class="_container"> 
           <?php
